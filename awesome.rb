dc7c12ac8a5ba6ed6b8fcd54588c27003c4ce96e
GarretRCorbett <GarretRCorbett@gmail.com>
2016-09-19 15:19:41 -0400

    Work Setup Commit
    Added Work Files

---
+++ awesome.rb
@@ -0,0 +1,8 @@
+puts "Ruby Is Awesome!"
+puts "Here are a vew computations to show why:"
+puts "2 + 5 = "
+puts 2 + 5
+puts "24 * 10 = "
+puts rand(0...100) * 10
+puts "Hi +  + There = "
+puts "Hi" + " " + "There"
---
+++ kings_strings.rb
@@ -0,0 +1,11 @@
+kings_string = "Property of His Royal Highness, the King"
+string_reflection = kings_string.reverse
+times_to_echo = 3
+string_echo = kings_string * 3
+puts kings_string
+puts string_reflection
+puts string_echo
+
+puts kings_string.length
+puts string_echo.length
+puts string_echo.length / times_to_echo
